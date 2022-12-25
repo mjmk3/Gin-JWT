44436d327bdf9334c3117a33fdcdcdaf9c1ecd9f
Mohamad J. Makki <malanbaki@transforat.com>
2022-12-25 16:45:10 +0300

    adding bcrypt

---
+++ client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"jwt-authentication-golang/model"
+	"log"
+)
+
+var Instance *gorm.DB
+var dbError error
+
+func Connect(connectionString string) {
+	Instance, dbError = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if dbError != nil {
+		log.Fatal(dbError)
+		panic("Cannot connect to DB")
+	}
+	log.Println("Connected to Database!")
+}
+func Migrate() {
+	Instance.AutoMigrate(&model.User{})
+	log.Println("Database Migration Completed!")
+}
--- main.go
+++ main.go
@@ -1,4 +1,6 @@
 package main
 
 func main() {
+	database.Connect("root:root@tcp(localhost:3306)/jwt_demo?parseTime=true")
+	database.Migrate()
 }
